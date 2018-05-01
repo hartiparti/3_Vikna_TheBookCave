cafd822cf78481c54bbbff2ce7716515c7573103
hartiparti <34035433+hartiparti@users.noreply.github.com>
2018-05-01 21:29:36 +0000

    Initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheBookCave.Models;
+
+namespace TheBookCave.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
